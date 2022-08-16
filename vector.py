47dc73185d98edcdb54232270e89501f356cc5da
m.ismael85@gmail.com <mohamed@lenovo.fritz.box>
2022-08-16 22:07:21 +0200

    vector init function

---
+++ vector.py
@@ -0,0 +1,13 @@
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError("The coordinates must be not empty")
+        
+        except TypeError:
+            raise TypeError("The coordinates must be iterable")
